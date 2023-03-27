8c3d64e430adebf9a5df3cebc43d192bb88d787c
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2023-03-27 19:10:27 +0300

    some minor fixes for current test scripts

---
+++ Descent_forPatchWork/Assets/Scripts/TurosScripts/Buttons/HeroButton.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class HeroButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public UnityEvent buttonClick;
+
+    private void Awake()
+    {
+        if(buttonClick == null)
+        {
+            buttonClick = new UnityEvent();
+        }
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("BUTTON DOWN!");
+
+        if (this.gameObject.CompareTag("Hero"))
+        {
+            GameObject statsPanel = this.gameObject.transform.Find("HeroStatsPanel").gameObject;
+
+            if (statsPanel.activeSelf == false)
+            {
+                statsPanel.SetActive(true);
+            }
+            else
+            {
+                statsPanel.SetActive(false);
+            }
+        }
+ 
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("BUTTON UP!");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        this.gameObject.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+}
