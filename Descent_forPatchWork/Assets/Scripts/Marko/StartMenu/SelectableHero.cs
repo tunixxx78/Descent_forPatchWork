1209a7bfb38b6ed2bb9f44ba023b9c26ae0a0dcf
wimarko <marko.wiik@metropolia.fi>
2023-04-19 10:49:04 +0300

    Savingsystem herostats similar to first play scene..

--- Descent_forPatchWork/Assets/Scripts/Marko/StartMenu/SelectableHero.cs
+++ Descent_forPatchWork/Assets/Scripts/Marko/StartMenu/SelectableHero.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class SelectableHero : MonoBehaviour
 {
 
+    public int plrIndex;
     public string heroName;
     public string heroRole;
 
@@ -16,8 +17,8 @@ public class SelectableHero : MonoBehaviour
     public int currentActionPoints;
     public int maxActionPoints;
 
-    public int atk;
-    public int def;
+    public int plrStrength;
+    public int plrShield;
     public int fate;
     public int exp;
 
